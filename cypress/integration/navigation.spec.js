55a8d41a7db5047f1a49ddbb8263dd44a48e2e67
James Brown <jamesbrown5292@gmail.com>
2020-10-08 00:57:53 +0000

    e2e tests implemented

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,5 +1,11 @@
 describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
+
   });
+
+ 
 });
